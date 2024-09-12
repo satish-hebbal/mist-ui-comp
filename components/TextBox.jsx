7db3eee566bb918921b329f2833244cb4e7609f0
satish-hebbal <satihhebbal96@gmail.com>
2024-09-12 17:53:56 +0530

    first commit

---
+++ components/TextBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TextBox() {
+  return (
+    <div className=''>
+      <h2 className='text-3xl font-mono text-rose-300'>namaste boi</h2>
+    </div>
+  )
+}
+
+export default TextBox
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import TextBox from '../components/TextBox'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <div>
+        namaste boi
+        <TextBox/>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
