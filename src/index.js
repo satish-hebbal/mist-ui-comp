d391ce8899f4a7e8ce9c6bbdec463bf8f68d7e76
satish-hebbal <satihhebbal96@gmail.com>
2024-09-16 14:11:03 +0530

    finally working!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-export { default as CheckBox } from '../components/CheckBox';
-export { default as FileUpload } from '../components/FileUpload';
-export { default as MultiSelectOption } from '../components/MultiSelectOption';
-export { default as RadioButton } from '../components/RadioButton';
-export { default as SelectDate } from '../components/SelectDate';
-export { default as SelectOption } from '../components/SelectOption';
-export { default as SubmitButton } from '../components/SubmitButton';
-export { default as TextArea } from '../components/TextArea';
-export { default as TextBox } from '../components/TextBox';
-export { default as ToggleSwitch } from '../components/ToggleSwitch';
+import './index.css';
+
+export { default as CheckBox } from './components/CheckBox';
+export { default as FileUpload } from './components/FileUpload';
+export { default as MultiSelectOption } from './components/MultiSelectOption';
+export { default as RadioButton } from './components/RadioButton';
+export { default as SelectDate } from './components/SelectDate';
+export { default as SelectOption } from './components/SelectOption';
+export { default as SubmitButton } from './components/SubmitButton';
+export { default as TextArea } from './components/TextArea';
+export { default as TextBox } from './components/TextBox';
+export { default as ToggleSwitch } from './components/ToggleSwitch';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'MistUIComp',
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  css: {
+    extract: true
+  }
 })
